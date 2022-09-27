c6187c7ea8886071b1e56b1e1cfc812bb751b8fd
code <code.sengel@gmail.com>
2022-09-28 00:53:06 +0200

    20220928_Authentification_1

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+
+        <q-toolbar-title>
+          {{AppName}}
+        </q-toolbar-title>
+
+
+      </q-toolbar>
+    </q-header>
+
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+
+
+export default defineComponent({
+  name: 'LoginLayout',
+
+  setup () {
+
+
+
+    var AppName = process.env.QUASAR_APP_NAME
+
+    return {
+      AppName
+    }
+  }
+})
+</script>
--- src/supabase.js
+++ src/supabase.js
@@ -6,3 +6,9 @@ const supabaseUrl = process.env.VUE_APP_SUPA_URL
 const supabaseAnonKey = process.env.VUE_APP_SUPA_ANON
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+
+export default function useSupabase (){
+  return {supabase}
+}
+
