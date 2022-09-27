de6ddf2e775c5485a907d9f6467bbd1f85a0508c
code <code.sengel@gmail.com>
2022-09-27 21:12:02 +0200

    first commit

---
+++ src/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+
+
+const supabaseUrl = process.env.VUE_APP_SUPA_URL
+const supabaseAnonKey = process.env.VUE_APP_SUPA_ANON
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
