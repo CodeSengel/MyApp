867a73a8bff1b4000a2bf1c4844cb87d9b7b6f7b
code <code.sengel@gmail.com>
2022-09-29 01:02:34 +0200

    20220329_Authentification_2

---
+++ src/pages/EmailConfirmation.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding class="row justify-center">
+    <p>
+      Thank you for your registration
+    </p>
+    <br>
+    <p>
+      We just sent an email to : <strong> {{$route.query.email}} </strong> please confirm to complete the registration.
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageEmailConfirmation'
+})
+</script>
